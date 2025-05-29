d65f858dd45602d662921d93861da8e0671c7d3e
Rathapp <povrathapp@gmail.com>
2025-05-29 20:14:01 +0700

    update nginx

--- superset_deploy/superset/config/superset_config.py
+++ superset_deploy/superset/config/superset_config.py
@@ -1,3 +1,9 @@
+from superset.config import * 
+
+SECRET_KEY = "flOZv9DVD3AZ-XyGPClEjvDiYfm6Pigiy6DTrTWsuLx0hJk6xxzp4aB36J5l8GC0l2DgjFZyG2M2Se_HJdFFXg"
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://superset:superset@postgres:5432/superset'
+RESULTS_BACKEND = RedisCache(host='redis', port=6379, key_prefix='superset_results') 
+
 
 APP_NAME = "NCHADS"
 APP_ICON = "/static/assets/images/logo.png"
@@ -15,9 +21,29 @@ RATELIMIT_STORAGE_URL = 'redis://redis:6379/0'
 #                     }
 #                     """
 # }
+
 FEATURE_FLAGS = {
     "ENABLE_TEMPLATE_PROCESSING": True,
     "ENABLE_REACT_CRUD_VIEWS": True,
     "ENABLE_REACT_USER_GROUPS": True,
 }
+
+# Flask-Caching Config
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_DEFAULT_TIMEOUT': 300,
+    'CACHE_KEY_PREFIX': 'superset_',
+    'CACHE_REDIS_URL': 'redis://redis:6379/0'
+}
+
+RESULTS_BACKEND = CACHE_CONFIG
+
+
+ENABLE_CORS = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = "Lax"
+
+# Optional: Set allowed origins, etc.
+ENABLE_PROXY_FIX = True
 
